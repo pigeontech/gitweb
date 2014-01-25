90de7982b761dd55f2bd68641be598d85b3b3dbd
Scott Pigeon <pigeontech@gmail.com>
2014-01-24 23:06:57 -0500

    first commit

---
+++ gitweb.php
@@ -0,0 +1,69 @@
+#!/usr/local/php54/bin/php
+<?php
+	// Output greeting info
+	greeting();
+
+	// Make sure STDIN is set
+	if (!defined("STDIN"))
+	{
+		define("STDIN", fopen('php://stdin','r'));
+	}
+
+	// Get username
+	$username = rtrim(shell_exec('whoami'),"\n");
+
+	// Get list of websites into array
+	exec('for i in $(ls -d */); do echo ${i%%/}; done', $websites);
+
+	// Ask which website, ask again if out of range
+	$choice = 0;
+	while ($choice < 1 || $choice > count($websites))
+	{
+		echo " Choose which website you want to manage with Git...\n\n";
+		for ($i = 0; $i < count($websites); $i++)
+		{
+			// don't list git repos!
+			//if (strpos($websites[$i], '.git') == false)
+			//{
+				echo '     #'.($i+1).' - '.$websites[$i]."\n";	
+			//}
+		}
+
+		echo "\n Type the number here: ";
+		$choice = (int)fread(STDIN, 80);
+
+		if ($choice < 1 || $choice > count($websites))
+		{
+			echo "\n That choice was out of range. Try again.\n\n";
+		}
+	}
+	
+	
+	//echo ' /home/'.$username.'/'.$websites[$choice-1]."\n";
+	//shell_exec("mkdir -p ".$websites[$choice-1].'.git');
+	//shell_exec("cd ".$websites[$choice-1].'.git');
+	$pr =
+		"#!/bin/sh\n".
+		"git --work-tree=/home/$username/".$websites[$choice-1]." --git-dir=/home/$username/.git/".$websites[$choice-1].".git checkout -f\n";
+
+	shell_exec("git init .git/".$websites[$choice-1].".git --bare");
+	shell_exec('echo "'.$pr.'" > .git/'.$websites[$choice-1].'.git/hooks/post-receive');
+	shell_exec('chmod +x .git/'.$websites[$choice-1].'.git/hooks/post-receive');
+
+
+	function greeting()
+	{
+		$greeting =
+		"\n".
+		" ----------------------------------- GitWeb -----------------------------------\n".
+		" ------------------------------------------------------------------------------\n".
+		" This program will help you create a git repository on your host, and then set\n".
+		" it up so that when you push updates to it, your web root will get updated too.\n".
+		" Obviously, only push updates you're okay with being on the LIVE website!\n".
+       	" ------------------------------------------------------------------------------\n\n";
+
+		echo $greeting;
+	}
+
+
+?>
